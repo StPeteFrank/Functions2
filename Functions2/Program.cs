70f58a4209ac696f2989a190d0d5e10b22a1a256
diazfrankp <diazfrankp@gmail.com>
2019-04-16 11:05:27 -0400

    SumArrayForLoop warm up.

--- Functions2/Program.cs
+++ Functions2/Program.cs
@@ -9,6 +9,7 @@ namespace Functions2
         {
             Console.WriteLine(new LargestInArray().Largest(new int[] { 2, 1, 4, 7, 10, 3, -7 }));
             Console.WriteLine(new SmallestInArray().Smallest(new int[] { 12, 2, 0, -9 }));
+            Console.WriteLine(new SumArrayForLoop().SumArray(new int[] { 10, 20, 30, 40 }));
 
             Console.ReadLine();
         }
---
+++ Functions2/SumArrayForLoop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Functions2
+{
+    public class SumArrayForLoop
+    {
+        public int SumArray(int [] a)
+        {
+            int sum = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum += a[i];
+            }
+            return sum;
+        }
+    }
+
+
+}
