20fdd14c0642d1af4acd4950e6fe9661ccb110c4
diazfrankp <diazfrankp@gmail.com>
2019-04-16 11:07:08 -0400

    SumArray using .Sum()

--- Functions2/SumArrayForLoop.cs
+++ Functions2/SumArrayForLoop.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Functions2
 {
-    public class SumArrayForLoop
+    /*public class SumArrayForLoop
     {
         public int SumArray(int [] a)
         {
@@ -15,7 +16,13 @@ namespace Functions2
             }
             return sum;
         }
+    }*/
+    public class SumArrayForLoop
+    {
+        public int SumArray(int [] a)
+        {
+            return a.Sum();
+        }
     }
-
 
 }
