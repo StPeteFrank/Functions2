00b9b03d538ec1ed5aa0cb14a65f7650aaec3f92
diazfrankp <diazfrankp@gmail.com>
2019-04-16 12:31:10 -0400

    Created SumNestedArray. Need input args in program. Error in output caused.

---
+++ Functions2/Classes/SumNestedForLoop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Functions2.Classes
+{
+    public class SumNestedForLoop
+    {
+        public int SumNested(int[][] a)
+        {
+            int sum = 0;
+            for(int i = 0; i < a.Length; i++)
+            {
+                for(int j = 0; j < a[i].Length; i++)
+                {
+                    sum += a[i][j];
+                }
+            }
+            return sum;
+        }
+    }
+}
+
