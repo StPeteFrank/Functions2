947a206f363c6046e1065d3a9ed3272bb1a61b97
diazfrankp <diazfrankp@gmail.com>
2019-04-15 19:04:45 -0700

    Test 2

--- TestForVSInstallGit/Program.cs
+++ TestForVSInstallGit/Program.cs
@@ -7,6 +7,7 @@ namespace TestForVSInstallGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
