61463fcbdac94410977d0e41a3e8a875a093bc08
diazfrankp <diazfrankp@gmail.com>
2019-04-17 11:26:44 -0400

    Test commit

--- Functions2/Classes/SumNestedForLoop.cs
+++ Functions2/Classes/SumNestedForLoop.cs
@@ -5,41 +5,20 @@ using System.Text;
 
 namespace Functions2.Classes
 {
-
-    /*public long SumNested(int[][] a)
-    {
-        long sum = 0;
-        for (int i = 0; i < a.Length; i++)
-        {
-            for (int j = 0; j < a[i].Length; i++)
-            {
-                sum += a[i][j];
-            }
-        }
-        return sum;
-    }*/
     public class SumNestedForLoop
-        {
-            public int SumNested(int [][] a)
-            {
-                int sum = 0;
-                for(int first = 0; first < a.Length; first++)
-                {
-                    for(int j = 0; j < a[first].Length; j++)
-                    {
-                        sum += a[first][j];
-                    }
-                }
-                return sum;
-            }
-        
-
-         /*public static int SumNested(int[][] a)
+    {
+         public int SumNested(int [][] a)
          {
-             return a.Sum(x => x.Sum());
-         }*/
-
-
+             int sum = 0;
+             for(int first = 0; first < a.Length; first++)
+             {
+                for(int j = 0; j < a[first].Length; j++)
+                {
+                  sum += a[first][j];
+                }
+             }
+             return sum;
+         }
     }
 }
 
