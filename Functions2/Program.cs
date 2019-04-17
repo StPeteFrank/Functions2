8174e68ddc88f261889b2808e2da0661149c0a87
diazfrankp <diazfrankp@gmail.com>
2019-04-17 10:41:16 -0400

    SumOddArrayForLoop daily warm up. Note negative numbers will not be counted as odd numbers.

--- Functions2/Program.cs
+++ Functions2/Program.cs
@@ -9,9 +9,9 @@ namespace Functions2
         {
             Console.WriteLine(new LargestInArray().Largest(new int[] { 5, 1, -2, 12, 9, 0 }));
             Console.WriteLine(new SmallestInArray().Smallest(new int[] { 9, 3, 0, -10, 8, -7, 12 }));
-            //Console.WriteLine(new SumArrayForLoop().SumArray(new int[] { 10, 20, 30, 40 }));
             Console.WriteLine(new SumArrayForLoop().SumArray(new int[] { 100, 200, 300, 400 }));
-            Console.WriteLine(new SumOddArrayForLoop().SumOddArray(new int[] { 2, 0, 3, 5, 7 }));
+            // Console.WriteLine(new SumOddArrayForLoop().SumOddArray(new int[] { 2, 0, 3, 5, 7 }));
+            Console.WriteLine(new SumOddArrayForLoop().SumOddArray(new int[] { 10, -1, 6, 3, 5, 7, 0, 1 }));
             Console.WriteLine(new SumArrayForEach().SumForEach(new int[] { 20, 40, 60, 80 }));
             Console.WriteLine(new SquareSumArray().SquareSum(new int[] { 2, 4, 5 }));
 
