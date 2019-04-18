f9f07836fe9a778038f642827bd5b1d9dc7257e7
diazfrankp <diazfrankp@gmail.com>
2019-04-18 08:31:06 -0400

    SumOddArray

--- Functions2/Classes/SumArrayForLoop.cs
+++ Functions2/Classes/SumArrayForLoop.cs
@@ -5,18 +5,6 @@ using System.Text;
 
 namespace Functions2
 {
-   /* public class SumArrayForLoop
-    {
-        public int SumArray(int [] a)
-        {
-            int sum = 0;
-            for(int i = 0; i < a.Length; i++)
-            {
-                sum += a[i];
-            }
-            return sum;
-        }
-    }*/
     public class SumArrayForLoop
     {
         public int SumArray(int [] a)
--- Functions2/Program.cs
+++ Functions2/Program.cs
@@ -9,9 +9,9 @@ namespace Functions2
         {
             Console.WriteLine(new LargestInArray().Largest(new int[] { 5, 1, -2, 12, 9, 0 }));
             Console.WriteLine(new SmallestInArray().Smallest(new int[] { 9, 3, 0, 12, 8, -10, 18, -11 }));
-            //Console.WriteLine(new SumArrayForLoop().SumArray(new int[] { 100, 200, 300, 400 }));
             Console.WriteLine(new SumArrayForLoop().SumArray(new int[] { 100, 200, 300, 400 }));
-            Console.WriteLine(new SumOddArrayForLoop().SumOddArray(new int[] { 10, -1, 6, 3, 5, 7, 0, 1 }));
+            //Console.WriteLine(new SumOddArrayForLoop().SumOddArray(new int[] { 10, -1, 6, 3, 5, 7, 0, 1 }));
+            Console.WriteLine(new SumOddArrayForLoop().SumOddArray(new int[] { 10, 6, 3, -3, 12, 5, 7, 0, 1 }));
             Console.WriteLine(new SumArrayForEach().SumForEach(new int[] { 200, 400, 600, 800 }));
             Console.WriteLine(new SquareSumArray().SquareSum(new int[] { 2, 4, 5 }));
             Console.WriteLine(new RemoveString().TakeOutString(new string("Fr!ank! is! gett!ing strong!er!!!")));
