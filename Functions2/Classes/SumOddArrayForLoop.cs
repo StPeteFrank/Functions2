5dfeb1e039ca1363c1b37b3b4ddc5d7c062f18f2
diazfrankp <diazfrankp@gmail.com>
2019-04-18 08:34:07 -0400

    SumForEach

--- Functions2/Classes/SumOddArrayForLoop.cs
+++ Functions2/Classes/SumOddArrayForLoop.cs
@@ -4,21 +4,6 @@ using System.Text;
 
 namespace Functions2.Classes
 {
-    /*public class SumOddArrayForLoop
-    {
-        public int SumOddArray(int [] a)
-        {
-            int sum = 0;
-            for (int i = 0; i < a.Length; i++)
-            {
-                if (a[i] % 2 == 1)
-                {
-                    sum += a[i];
-                }
-            }
-            return sum;
-        }
-    }*/
     public class SumOddArrayForLoop
     {
         public int SumOddArray(int [] a)
