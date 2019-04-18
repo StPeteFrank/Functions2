59dc939f1d4ee209613c0e96b3ad9c37f2625a92
diazfrankp <diazfrankp@gmail.com>
2019-04-18 08:26:39 -0400

    SumArrForLoop

--- Functions2/Program.cs
+++ Functions2/Program.cs
@@ -9,7 +9,7 @@ namespace Functions2
         {
             Console.WriteLine(new LargestInArray().Largest(new int[] { 5, 1, -2, 12, 9, 0 }));
             Console.WriteLine(new SmallestInArray().Smallest(new int[] { 9, 3, 0, 12, 8, -10, 18, -11 }));
-            //Console.WriteLine(new SmallestInArray().Smallest(new int[] { 9, 3, 0, -10, 8, -7, 12 }));
+            //Console.WriteLine(new SumArrayForLoop().SumArray(new int[] { 100, 200, 300, 400 }));
             Console.WriteLine(new SumArrayForLoop().SumArray(new int[] { 100, 200, 300, 400 }));
             Console.WriteLine(new SumOddArrayForLoop().SumOddArray(new int[] { 10, -1, 6, 3, 5, 7, 0, 1 }));
             Console.WriteLine(new SumArrayForEach().SumForEach(new int[] { 200, 400, 600, 800 }));
