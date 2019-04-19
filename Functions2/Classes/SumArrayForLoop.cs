6c6c03c68c0ae71189b2a15ee8ebce0149a80180
diazfrankp <diazfrankp@gmail.com>
2019-04-19 12:56:25 -0400

    SumArray warm up

--- Functions2/Classes/SumArrayForLoop.cs
+++ Functions2/Classes/SumArrayForLoop.cs
@@ -5,12 +5,24 @@ using System.Text;
 
 namespace Functions2
 {
-    public class SumArrayForLoop
+    /*public class SumArrayForLoop
     {
         public int SumArray(int [] a)
         {
             int sum = 0;
             for (int i = 0; i < a.Length; i++)
+            {
+                sum += a[i];
+            }
+            return sum;
+        }
+    }*/
+    public class SumArrayForLoop
+    {
+        public int SumArray(int [] a)
+        {
+            int sum = 0;
+            for(int i = 0; i < a.Length; i++)
             {
                 sum += a[i];
             }
