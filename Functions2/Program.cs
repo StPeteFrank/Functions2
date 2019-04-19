6258a4c32bc156264be0855cd0c1d8d1b18a4196
diazfrankp <diazfrankp@gmail.com>
2019-04-19 13:00:53 -0400

    SumOddArray dialy

--- Functions2/Program.cs
+++ Functions2/Program.cs
@@ -9,9 +9,9 @@ namespace Functions2
         {
             Console.WriteLine(new LargestInArray().Largest(new int[] { 5, 0, -3, 9, 4, 12, -20 }));
             Console.WriteLine(new SmallestInArray().Smallest(new int[] { 10, 0, -4, 16, 10, 12, -9, -7 }));
-            //Console.WriteLine(new SumArrayForLoop().SumArray(new int[] { 100, 200, 300, 400 }));
             Console.WriteLine(new SumArrayForLoop().SumArray(new int[] { 100, 200, 300, 400 }));
-            Console.WriteLine(new SumOddArrayForLoop().SumOddArray(new int[] { 10, 6, 3, -3, 12, 5, 7, 0, 1 }));
+            //Console.WriteLine(new SumOddArrayForLoop().SumOddArray(new int[] { 10, 6, 3, -3, 12, 5, 7, 0, 1 }));
+            Console.WriteLine(new SumOddArrayForLoop().SumOddArray(new int[] { 9, 0, -3, 3, 12, 10, 5, 7, 100 }));
             Console.WriteLine(new SumArrayForEach().SumForEach(new int[] { 200, 400, 600, 800 }));
             Console.WriteLine(new SquareSumArray().SquareSum(new int[] { 2, 4, 5 }));
             Console.WriteLine(new RemoveString().Remove(new string("Fra!nk is! gett!ing s!tronger!!!")));
