ddcc32240460db2328333d91fb82b2e32eb003f8
diazfrankp <diazfrankp@gmail.com>
2019-04-22 09:32:32 -0400

    RemoveString

--- Functions2/Classes/RemoveString.cs
+++ Functions2/Classes/RemoveString.cs
@@ -4,6 +4,13 @@ using System.Text;
 
 namespace Functions2.Classes
 {
+    /*public class RemoveString
+    {
+        public string RemoveIt(string a)
+        {
+            return a.Replace("!", "") + "!";
+        }
+    }*/
     public class RemoveString
     {
         public string RemoveIt(string a)
