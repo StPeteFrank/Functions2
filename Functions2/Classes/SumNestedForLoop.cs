fbae96af93adb778f56e753fe8eccd326a8ad85d
diazfrankp <diazfrankp@gmail.com>
2019-04-23 14:10:13 -0400

    SumNestedArray daily.

--- Functions2/Classes/SumNestedForLoop.cs
+++ Functions2/Classes/SumNestedForLoop.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Functions2.Classes
 {
-    public class SumNestedForLoop
+    /*public class SumNestedForLoop
     {
         public int NestedArray(int [][] a)
         {
@@ -18,6 +18,21 @@ namespace Functions2.Classes
                 }
             }
             return sum;
+        }
+    }*/
+    public class SumNestedForLoop
+    {
+        public int SumNestedArray(int [][] a)
+        {
+            int sum = 0;
+            for(int j = 0; j < a.Length; j++)
+            {
+                for(int k = 0; k < a[j].Length; k++)
+                {
+                    sum += a[j][k];
+                }
+            }
+            return sum; 
         }
     }
 
