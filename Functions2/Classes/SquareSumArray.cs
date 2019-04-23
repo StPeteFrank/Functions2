2180708805a5705cfefa673071c6396d503ce4da
diazfrankp <diazfrankp@gmail.com>
2019-04-23 14:04:43 -0400

    RemoveString daily

--- Functions2/Classes/SquareSumArray.cs
+++ Functions2/Classes/SquareSumArray.cs
@@ -4,19 +4,6 @@ using System.Text;
 
 namespace Functions2
 {
-   /* public class SquareSumArray
-    {
-        public int SquareSum(int [] a)
-        {
-            int sum = 0;
-            for (int i = 0; i < a.Length; i++)
-            {
-                sum += a[i] * a[i];
-            }
-            return sum;
-        }
-    }*/
-
     public class SquareSumArray
     {
         public int SquareSum(int [] a)
