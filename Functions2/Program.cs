d5c6acaf0c7bcce1dd3a249f29e0d775a92ae70b
diazfrankp <diazfrankp@gmail.com>
2019-04-23 13:34:26 -0400

    LargestInArray warm up.

--- Functions2/Program.cs
+++ Functions2/Program.cs
@@ -7,14 +7,14 @@ namespace Functions2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(new LargestInArray().Largest(new int[] { 5, 0, -3, 11, 9, -14 }));
+            // Console.WriteLine(new LargestInArray().Largest(new int[] { 5, 0, -3, 11, 9, -14 }));
+            Console.WriteLine(new LargestInArray().Largest(new int[] { 5, 0, -2, 100, 70, 2 }));
             Console.WriteLine(new SmallestInArray().Smallest(new int[] { 2, 0, 9, 12, -3, 16, -9, 100 }));
             Console.WriteLine(new SumArrayForLoop().SumArray(new int[] { 100, 200, 300, 400}));
             Console.WriteLine(new SumOddArrayForLoop().SumOddArray(new int[] { 9, 0, -3, 3, 12, 10, 5, 7, 100 }));
             Console.WriteLine(new SumArrayForEach().SumForEach(new int[] { 200, 400, 600, 800 }));
             Console.WriteLine(new SquareSumArray().SquareSum(new int[] { 2, 4, 5 }));
             Console.WriteLine(new RemoveString().RemoveIt(new string("!After th!is w!e! wil!l pract!ice s!ome r!e!a!c!t!!!!")));
-            //Console.WriteLine(new SumNestedForLoop().NestedForLoop(new int[][] { new int[] { 2, 4, 6 }, new int[] { 3, 5, 7 } }));
             Console.WriteLine(new SumNestedForLoop().NestedArray(new int[][] { new int[] { 2, 4, 6 }, new int[] { 3, 5, 7 } }));
 
 
