ea7bb3a6ffab034590b0e7cd5f756001faf56dc1
diazfrankp <diazfrankp@gmail.com>
2019-04-29 09:52:59 -0400

    LargestInArray object, testing github .

--- Functions2/Classes/SumNestedForLoop.cs
+++ Functions2/Classes/SumNestedForLoop.cs
@@ -5,21 +5,6 @@ using System.Text;
 
 namespace Functions2.Classes
 {
-    /*public class SumNestedForLoop
-    {
-        public int SumNestedArray(int [][] a)
-        {
-            int sum = 0;
-            for(int j = 0; j < a.Length; j++)
-            {
-                for(int k = 0; k < a[j].Length; k++)
-                {
-                    sum += a[j][k];
-                }
-            }
-            return sum; 
-        }
-    }*/
     public class SumNestedForLoop
     {
         public int SumNested(int [][] a)
--- Functions2/Program.cs
+++ Functions2/Program.cs
@@ -7,17 +7,14 @@ namespace Functions2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(new LargestInArray().Largest(new int[] { 5, 0, -3, 11, 9, -14 }));
+            //Console.WriteLine(new LargestInArray().Largest(new int[] { 5, 0, -3, 11, 9, -14 }));
+            Console.WriteLine(new LargestInArray().Largest(new int[] { 10, 7, -3, 0, 12, 9 }));
             Console.WriteLine(new SmallestInArray().Smallest(new int[] { 2, 10, -9, -10, 12 }));
             Console.WriteLine(new SumArrayForLoop().SumArray(new int[] { 100, 200, 300, 400 }));
             Console.WriteLine(new SumOddArrayForLoop().SumOddArray(new int[] { 2, 0, 5, 10, 7, 3, -9 }));
             Console.WriteLine(new SumArrayForEach().SumForEach(new int[] { 200, 400, 600, 800 }));
             Console.WriteLine(new SquareSumArray().SquareSum(new int[] { 2, 4, 5 }));
-            //Console.WriteLine(new RemoveString().Remove(new string("W!e fee!l! !good! ab!!out !!our i!n!t!e!r!v!i!e!w!!!")));
             Console.WriteLine(new RemoveString().RemoveIt(new string("!Th!is !is! a! te!!!st")));
-
-            //Console.WriteLine(new SumNestedForLoop().SumNestedArray(new int[][] { new int[] { 2, 4, 6 }, new int[] { 3, 5, 7 } }));
-
             Console.WriteLine(new SumNestedForLoop().SumNested(new int[][] { new int[] { 2, 4, 6 }, new int[] { 3, 5, 7 } }));
                 
 
