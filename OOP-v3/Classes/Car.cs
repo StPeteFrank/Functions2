4555ae25b668e36a2e89b16062544f15f21c3f7e
diazfrankp <diazfrankp@gmail.com>
2019-04-30 11:23:27 -0400

    Car parent class set. Constructor set.

---
+++ OOP-v3/Classes/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_v3.Classes
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int HorsePower { get; set; }
+
+        public Car(string make, string model, int horsePower)
+        {
+            Make = make;
+            Model = model;
+            HorsePower = horsePower;
+        }
+    }
+}
