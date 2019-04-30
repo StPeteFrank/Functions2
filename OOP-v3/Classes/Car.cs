29ad76c9d91c6b77d0f127009bc2a45282bf9c16
diazfrankp <diazfrankp@gmail.com>
2019-04-30 11:37:06 -0400

    Car parent class inherits IManageHP and implements methods.

--- OOP-v3/Classes/Car.cs
+++ OOP-v3/Classes/Car.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace OOP_v3.Classes
 {
-    public class Car
+    public class Car : IManageHP
     {
         public string Make { get; set; }
         public string Model { get; set; }
@@ -19,6 +19,16 @@ namespace OOP_v3.Classes
         public override string ToString()
         {
             return $"This is a {this.Make} {this.Model} and it has {this.HorsePower} bhp.";
+        }
+
+        public void IncreaseHP(int increase)
+        {
+            this.HorsePower += increase;
+        }
+
+        public void DecreaseHP(int decrease)
+        {
+            this.HorsePower -= decrease;
         }
     }
 }
---
+++ OOP-v3/Classes/IManageHP.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_v3.Classes
+{
+    public interface IManageHP
+    {
+        int HorsePower { get; set; }
+        void IncreaseHP(int increase);
+        void DecreaseHP(int decrease);
+    }
+}
