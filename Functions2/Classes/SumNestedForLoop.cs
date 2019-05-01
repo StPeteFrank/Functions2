9618c72aadd6cd740af27691db4e3c244407e335
diazfrankp <diazfrankp@gmail.com>
2019-05-01 11:39:55 -0400

    Added CarPrototype and pedac for abstarct class. Focus on creating sub classes that use dependency injection.

--- Functions2/Classes/SumNestedForLoop.cs
+++ Functions2/Classes/SumNestedForLoop.cs
@@ -5,24 +5,6 @@ using System.Text;
 
 namespace Functions2.Classes
 {
-    /*public class SumNestedForLoop
-    {
-        public int NestedLoop(int [][] a)
-        {
-            int sum = 0;
-            for(int i = 0; i < a.Length; i++)
-            {
-                for(int j = 0; j < a[i].Length; j++)
-                {
-                    
-                    
-                        sum += a[i][j];
-                    
-                }
-            }
-            return sum;
-        }
-    }*/
     public class SumNestedForLoop
     {
         public int NestedLoop(int [][] a)
---
+++ OOP-v3/Classes/CarPrototype.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_v3.Classes
+{
+
+    //Make a showroom of cars that have sportcars, trucks, and sedans
+    //sportcars increase hp and increase value
+    //trucks increase hp and decrease value
+    //sedans decrease hp and decrease value
+
+
+    public class CarPrototype
+    {
+        public string Make { get; set; }   //Set the properties.
+        public string Model { get; set; }
+        public int HorsePower { get; set; }
+        public int CarValue { get; set; }
+    }
+}
