c74e1d3181adb2d0fd9a49f8e39e494ddc85f39f
diazfrankp <diazfrankp@gmail.com>
2019-05-02 10:55:30 -0400

    AggregateStringTest

--- Functions2/Classes/AggregateStringTest.cs
+++ Functions2/Classes/AggregateStringTest.cs
@@ -5,11 +5,18 @@ using System.Text;
 
 namespace Functions2.Classes
 {
-    public class AggregateStringTest
+    /*public class AggregateStringTest
     {
         public string AggregateString(string [] mySkills)
         {
             return mySkills.Aggregate((s1, s2) => s1 + ", " + s2);
+        }
+    }*/
+    public class AggregateStringTest
+    {
+        public string AggregateString(string [] a)
+        {
+            return a.Aggregate((b, c) => b + ", " + c) + " these are my skills";
         }
     }
 }
--- Functions2/Classes/MultiplyArrayForLoop.cs
+++ Functions2/Classes/MultiplyArrayForLoop.cs
@@ -5,43 +5,24 @@ using System.Text;
 
 namespace Functions2.Classes
 {
-    /*public class MultiplyArrayForLoop
-    {
-        public int MultiplyArray(int [] a)
-        {
-            int b = 1;
-            for(int i = 0; i < a.Length; i++)
-            {
-                b *= a[i];
-            }
-            return b;
-        }
-    }*/
-    /*public class MultiplyArrayForLoop
-    {
-        public int MultiplyArray(int [] a)
-        {
-            return a.Aggregate((b, c) => b * c);
-        }
-    }*/
-    /*public class MultiplyArrayForLoop
-    {
-        public int MultiplyArray(int [] a)
-        {
-            int b = 1;
-            for(int i = 0; i < a.Length; i++)
-            {
-                b *= a[i];
-            }
-            return b;
-        }
-    }*/
     public class MultiplyArrayForLoop
     {
         public int MultiplyArray(int [] a)
         {
-            return a.Aggregate((b, c) => b * c);
+            int b = 1;
+            for(int i = 0; i < a.Length; i++)
+            {
+                b *= a[i];
+            }
+            return b;
         }
     }
+    /*public class MultiplyArrayForLoop
+    {
+        public int MultiplyArray(int [] a)
+        {
+            return a.Aggregate((b, c) => b * c);
+        }
+    }*/
 
 }
