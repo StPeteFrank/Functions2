7b625b0d9fd6aa48dea56dcf981ac65b5ed01c3e
diazfrankp <diazfrankp@gmail.com>
2019-05-12 16:18:13 -0400

    Encapsulated Book class. Ctor uses List of doubles. AddGrades and ShowStatistics methods. and output logic using System Math methods in a loop.

---
+++ GradeBook/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook
+{
+    public class Book
+    {
+        public Book(string name)    // Explicit ctor (constructor method) which is with in the class. Implicit would be when the compiler creates a default ctor.
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+        public void AddGrade(double grade)  // Method.
+        {
+            grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+            foreach (var grade in grades)
+            {
+                lowGrade = Math.Min(grade, lowGrade);
+                highGrade = Math.Max(grade, highGrade);
+                result += grade;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}. My favorite score was {grades[0]}." +
+                $" The highest grade was {highGrade}. Lowest was {lowGrade}.");  //N1 is a formatting specifier
+        }
+
+        private List<double> grades;    //This is a field. outside the method. No using implicit types.
+        private string name;
+    }
+}
--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -2,34 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 
+
 namespace GradeBook
 {
     class Program
     {
         static void Main(string[] args)  //Main method   //DLL dynamic link library   //cw , hit tab twice for auto
         {
-            //var numbers = new[] { 10.3, 12.7, 6.11, 4.1};
-            var grades = new List<double> { 10.3, 12.7, 6.11, 4.1 };
-            grades.Add(12.09);
-           
 
-            var result = 0.0;
-            foreach(var grade in grades)
-            {
-                result += grade;
-            }
-            result /= grades.Count;
-            Console.WriteLine($"The average grade is {result:N1}. My favorite score was {grades[0]}");  //N1 is a formatting specifier
-            
+
+            var book = new Book("Frank's Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(14.02);
+            book.AddGrade(65.07);
+            book.ShowStatistics();
            
-           if (args.Length > 0) 
+          /* if (args.Length > 0) 
             {
                 Console.WriteLine($"Hello {args[0]}!");
             }
             else
             {
                 Console.WriteLine("Hello");
-            }
+            }*/
           
             Console.ReadLine();
         }
