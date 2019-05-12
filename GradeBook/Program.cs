5e2ef7c54d78beacb6983999428308548bacc3c2
diazfrankp <diazfrankp@gmail.com>
2019-05-12 14:45:51 -0400

    Variables, arrays, lists, count logic.

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -1,13 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GradeBook
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)  //Main method   //DLL dynamic link library   //cw , hit tab twice for auto
         {
+            //var numbers = new[] { 10.3, 12.7, 6.11, 4.1};
+            var grades = new List<double> { 10.3, 12.7, 6.11, 4.1 };
+            grades.Add(12.09);
+           
 
-            Console.WriteLine("Hello World!");
+            var result = 0.0;
+            foreach(var grade in grades)
+            {
+                result += grade;
+            }
+            result /= grades.Count;
+            Console.WriteLine($"The average grade is {result:N1}. My favorite score was {grades[0]}");  //N1 is a formatting specifier
+            
+           
+           if (args.Length > 0) 
+            {
+                Console.WriteLine($"Hello {args[0]}!");
+            }
+            else
+            {
+                Console.WriteLine("Hello");
+            }
+          
+            Console.ReadLine();
         }
     }
 }
